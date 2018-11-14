f8755a75775b69d0b00f142bbf4375637d8b37e3
cheeriotb <cheerio.the.bear@gmail.com>
2018-11-14 20:51:40 +0900

    Support the instruction bytes 0x06, 0x0A, 0x08 and 0x0C
    Some code was added to support the case 1, case 2, case 3 and case 4 in
    accordance with the test specification for OmapiTest.testTransmitApdu().

--- src/com/github/cheeriotb/cts/cardlet/OmapiApplet.java
+++ src/com/github/cheeriotb/cts/cardlet/OmapiApplet.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2018 cheeriotb
+ *  Copyright (C) 2018 cheeriotb <cheerio.the.bear@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -20,10 +20,16 @@ package com.github.cheeriotb.cts.cardlet;
 
 import javacard.framework.APDU;
 import javacard.framework.Applet;
+import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
 import javacard.framework.Util;
 
 public class OmapiApplet extends Applet {
+
+    final private static byte INS_BASIC_CLASS_1 = (byte) 0x06;
+    final private static byte INS_BASIC_CLASS_3 = (byte) 0x0A;
+    final private static byte INS_BASIC_CLASS_2 = (byte) 0x08;
+    final private static byte INS_BASIC_CLASS_4 = (byte) 0x0C;
 
     final private static byte[] FCI_TEMPLATE = {
             (byte) 0x6F, (byte) 0x0A, (byte) 0x64, (byte) 0x03, (byte) 0x53, (byte) 0x01,
@@ -43,6 +49,38 @@ public class OmapiApplet extends Applet {
         if (selectingApplet()) {
             Util.arrayCopy(FCI_TEMPLATE, (short) 0, buffer, (short) 0, (short) FCI_TEMPLATE.length);
             apdu.setOutgoingAndSend((short) 0, (short) FCI_TEMPLATE.length);
+            return;
+        }
+        byte[] output = null;
+        switch (buffer[ISO7816.OFFSET_INS]) {
+            case INS_BASIC_CLASS_1:
+            case INS_BASIC_CLASS_3:
+                if (buffer[ISO7816.OFFSET_P1] != 0x00 || buffer[ISO7816.OFFSET_P2] != 0x00) {
+                    ISOException.throwIt(ISO7816.SW_WRONG_P1P2);
+                }
+                if (buffer[ISO7816.OFFSET_INS] == INS_BASIC_CLASS_3
+                        && apdu.setIncomingAndReceive() != 0x01) {
+                    ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+                }
+                break;
+            case INS_BASIC_CLASS_2:
+            case INS_BASIC_CLASS_4:
+                if (buffer[ISO7816.OFFSET_P1] != 0x00 || buffer[ISO7816.OFFSET_P2] != 0x00) {
+                    ISOException.throwIt(ISO7816.SW_WRONG_P1P2);
+                }
+                if (buffer[ISO7816.OFFSET_INS] == INS_BASIC_CLASS_4
+                        && apdu.setIncomingAndReceive() != 0x01) {
+                    ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+                }
+                output = new byte[256];
+                Util.arrayFillNonAtomic(output, (short) 0, (short) output.length, (byte) 0x00);
+                apdu.setOutgoing();
+                apdu.setOutgoingLength((short) output.length);
+                apdu.sendBytesLong(output, (short) 0, (short) output.length);
+                break;
+            default:
+                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+                break;
         }
     }
 }
